03d3e6041cee007809a9133d4fdc06e99c69b0e9
araujoo77 <elivanlimadearaujo@gmail.com>
2025-06-02 14:06:43 +0000

    Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import {Routes, Route} from 'react-router-dom'
 import Home from './Home'
 
+import Menu from './Menu'
+
 const App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
+      <Route index element={<Menu/>} />
+      <Route path="/home" element={<Home/>} />
+
     </Routes>
   )
 }
